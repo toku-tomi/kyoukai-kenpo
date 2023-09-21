129e9bd6f2b7931208146710145a41294822eccc
tdomy <y.tokudomi@tokutomi.jp>
2023-09-21 21:53:31 +0900

    chore: change eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,27 @@
 module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["standard-with-typescript", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "extends": [
-        "standard-with-typescript",
-        "prettier"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "ignorePatterns": [".eslintrc.js"],
-    "rules": {
-        "@typescript-eslint/semi": ["error", "always"],
-    }
-}
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  ignorePatterns: [".eslintrc.js"],
+  rules: {
+    "@typescript-eslint/semi": ["error", "always"],
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+  },
+};
