aeb922e5bb379e696a01b9a0f38238f29f9b39f2
tdomy <y.tokudomi@tokutomi.jp>
2023-09-25 11:34:52 +0900

    fix: add prefix to api path

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import * as fs from "node:fs/promises";
 import * as path from "path";
 import { copy } from "fs-extra";
-import { payGradesJson } from "@/json-pages/pay-grades";
-import { prefecturesJson } from "./json-pages/prefectures";
-import { termsJson } from "./json-pages/terms";
-import { ratesJson } from "./json-pages/terms.[term].rates";
+import { payGradesJson } from "@/json-pages/v1/pay-grades.[gradeType]";
+import { prefecturesJson } from "./json-pages/v1/prefectures";
+import { termsJson } from "./json-pages/v1/terms";
+import { ratesJson } from "./json-pages/v1/terms.[term].rates";
 
 async function main(): Promise<void> {
   const artifactsDir = path.join(__dirname, "..", "dist");
@@ -20,17 +20,19 @@ async function main(): Promise<void> {
   // JSONファイル生成
   const results = [];
 
-  // 等級区分 /pay-grades/{type}
-  results.push(payGradesJson(artifactsDir));
+  const v1Dir = path.join(artifactsDir, "v1");
 
-  // 都道府県 /prefectures
-  results.push(prefecturesJson(artifactsDir));
+  // 報酬等級区分 /v1/pay-grades/[gradeType]/
+  results.push(payGradesJson(v1Dir));
 
-  // 期間 /terms
-  results.push(termsJson(artifactsDir));
+  // 都道府県 /v1/prefectures/
+  results.push(prefecturesJson(v1Dir));
 
-  // 料率 /rates
-  results.push(ratesJson(artifactsDir));
+  // 期間 /v1/terms/
+  results.push(termsJson(v1Dir));
+
+  // 料率 /v1/terms/[term]/rates/
+  results.push(ratesJson(v1Dir));
 
   await Promise.all(results);
 }
